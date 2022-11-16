1f72b09d19ead07196ad57a8fdbe2194f5566b8b
technorahul <rahul.sahu@technoloader.com>
2022-11-16 17:35:32 +0530

    Feat: Initail commit

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+use App\Models\User;
+use App\Models\Role;
+use App\Models\Permission;
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            'name'  => 'User',
+            'email' => 'user@user.com',
+            'phone' => '9988776655',
+            'dob' => '1996/01/01',
+            'password' => bcrypt('password')
+        ];
+        User::create($user);
+
+        $admin = [
+            ['name'  => 'Admin','email' => 'admin@admin.com','password' =>bcrypt('password'),'phone' => '9988776655','dob' => '1996/01/01'],
+            ['name'  => 'Editor','email' => 'editor@editor.com','password' =>bcrypt('password'),'phone' => '9988776655','dob' => '1996/01/01'],
+            ['name'  => 'Author','email' => 'author@author.com','password' =>bcrypt('password'),'phone' => '9988776655','dob' => '1996/01/01'],
+        ];
+        Admin::insert($admin);
+
+        Role::insert([
+            ['name'=>'Admin','slug'=>'admin'],
+            ['name'=>'Editor','slug'=>'editor'],
+            ['name'=>'Author','slug'=>'author'],
+        ]);
+
+        Permission::insert([
+            ['name'=>'Add Post','slug'=>'add-post'],
+            ['name'=>'Delete Post','slug'=>'delete-post'],
+        ]);
+
+        // Assign Role
+        Admin::whereId(1)->first()->roles()->attach([1]);
+        Admin::whereId(2)->first()->roles()->attach([2]);
+        Admin::whereId(3)->first()->roles()->attach([3]);
+
+        // Role has Permission
+        Role::whereId(1)->first()->permissions()->attach([1,2]);
+        Role::whereId(2)->first()->permissions()->attach([1]);
+        Role::whereId(3)->first()->permissions()->attach([1]);
+
+    }
+}
